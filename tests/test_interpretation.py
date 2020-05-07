e009cce458b6e915e4a9a7972e3c06acfbd12411
Masashi Kurita <marty.marron@gmail.com>
2020-05-07 16:18:22 +0900

    move test cases which verify actual annotation process to test_annotate_utterance.py

--- tests/test_interpretation.py
+++ tests/test_interpretation.py
@@ -1,67 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from nlu_annotation_helper import Interpretation
+from nlu_annotation_helper import AnnotateUtterance
 
 
-def test_get_annotated_utterance_with_labeled_tokens_only():
-    domain = "HomeAutomation"
+def test_get_annotated_utterance_with_any_intent(mocker):
+    dummy_annotated_result = ("annotated text", ["label"])
+    mocker.patch.object(AnnotateUtterance, "annotate", return_value=dummy_annotated_result)
+
     intent = "TurnOffApplianceIntent"
-    utterance = "テレビ 消し て"
-    slot_values = {
-        "ActionTrigger": "消して",
-        "DeviceType": "テレビ",
-        "Appliance": "テレビ"
-    }
-    interp = Interpretation(domain, intent, utterance, slot_values)
-    interp.set_lang("ja")
+    interp = Interpretation(domain="", intent=intent, utterance="", slot_values={})
 
-    expected = ("{テレビ|DeviceType}{消して|ActionTrigger}", ["DeviceType", "ActionTrigger"])
-    actual = interp.get_annotated_utterance()
-
-    assert actual == expected
-
-
-def test_get_annotated_utterance_with_non_labaled_token():
-    domain = "HomeAutomation"
-    intent = "TurnOffApplianceIntent"
-    utterance = "テレビ を 消し て"
-    slot_values = {
-        "ActionTrigger": "消して",
-        "DeviceType": "テレビ",
-        "Appliance": "テレビ"
-    }
-    interp = Interpretation(domain, intent, utterance, slot_values)
-    interp.set_lang("ja")
-
-    expected = ("{テレビ|DeviceType}を{消して|ActionTrigger}", ["DeviceType", "ActionTrigger"])
-    actual = interp.get_annotated_utterance()
-
-    assert actual == expected
-
-
-def test_get_annotated_utterance_with_non_labeled_token_only():
-    domain = "Global"
-    intent = "StopIntent"
-    utterance = "止め て"
-    slot_values = {}
-    interp = Interpretation(domain, intent, utterance, slot_values)
-    interp.set_lang("ja")
-
-    expected = ("止めて", [])
-    actual = interp.get_annotated_utterance()
-
-    assert actual == expected
-
-
-def test_get_annotated_utterance_with_non_labeled_one_character_token():
-    domain = "Global"
-    intent = "NextIntent"
-    utterance = "次"
-    slot_values = {}
-    interp = Interpretation(domain, intent, utterance, slot_values)
-    interp.set_lang("ja")
-
-    expected = ("次", [])
+    expected = dummy_annotated_result
     actual = interp.get_annotated_utterance()
 
     assert actual == expected
