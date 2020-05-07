ac7e15445e1ac96d41560a09ea26106fe02ebcd1
Masashi Kurita <marty.marron@gmail.com>
2020-05-07 18:00:02 +0900

    add pytest_mock package to use mocking in unit tests

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     install_requires=["langdetect"],
     extras_require={
         "dev": [
-            "pytest"
+            "pytest",
+            "pytest_mock"
         ]
     },
     entry_points="""\
